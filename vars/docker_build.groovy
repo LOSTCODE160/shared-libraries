bf1bcd00b61a6dc30c1ce3947ba5e3bcbb11381a
vithal <115823274+LOSTCODE160@users.noreply.github.com>
2026-07-16 22:44:52 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def call(String ProjectName,String ImageTag String DockerHubUser){
+def call(String ProjectName,String ImageTag,String DockerHubUser){
  sh " docker build  -t ${DockerHubUser}/${ProjectName}:${ImageTag} . "
  echo 'this the build stage' 
 }
