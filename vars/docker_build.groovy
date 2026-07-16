01be34166833651d475db204b64e986555de1f5b
vithal <115823274+LOSTCODE160@users.noreply.github.com>
2026-07-16 22:31:23 +0530

    Fix syntax errors in docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,6 +1,6 @@
 def call(String ProjectName,string ImageTag string DockerHubUser){
-sh " docker build  -t ${DockerHubUser}/${ProjectName}:${ImageTag} . "
-echo 'this the build stage' 
+ sh " docker build  -t ${DockerHubUser}/${ProjectName}:${ImageTag} . "
+ echo 'this the build stage' 
 }
 
 
