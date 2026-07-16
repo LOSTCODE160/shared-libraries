16136ed845ad81d48cd3c48c73ca6f0b9d31811e
vithal <115823274+LOSTCODE160@users.noreply.github.com>
2026-07-16 21:44:26 +0530

    Update git-clone.groovy

--- vars/git-clone.groovy
+++ vars/git-clone.groovy
@@ -1,5 +1,7 @@
-def call(string url ,string  branch) {
-   branch: ${branch},
-   url: ${url}
-   echo 'the code has been cloned from the source'
+def call(String branch, String url) {
+
+    git branch: branch,
+        url: url
+
+    echo 'the code has been cloned from the source'
 }
